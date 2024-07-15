a76194d5e1866edd5f8c571b1e13f22a16b28564
fmway <fm18lv@gmail.com>
2024-07-15 07:28:09 +0700

    update config in phpmyadmin

--- phpmyadmin/config.inc.php
+++ phpmyadmin/config.inc.php
@@ -7,13 +7,13 @@
  * or at <https://docs.phpmyadmin.net/>.
  */
 
-declare(strict_types=1);
+/*declare(strict_types=1);*/
 
 /**
  * This is needed for cookie based authentication to encrypt the cookie.
  * Needs to be a 32-bytes long string of random bytes. See FAQ 2.10.
  */
-$cfg['blowfish_secret'] = ''; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
+$cfg['blowfish_secret'] = 'theExampleWrites16ValuesHere'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
 
 /**
  * Servers configuration
