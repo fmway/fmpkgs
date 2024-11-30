b7e0ca3c1de8cdb4cb3ce452c9987044e4e51d0b
fmway <fm18lv@gmail.com>
2024-11-30 13:48:23 +0700

    fix: ...

--- firefox-addons/firefoxAddonsUpdater.nix
+++ firefox-addons/firefoxAddonsUpdater.nix
@@ -1,19 +1,12 @@
 { pkgs, lib, ... }:
-pkgs.stdenvNoCC.mkDerivation {
-  name = "firefox-updater";
-  src = pkgs.writeScriptBin /* fish */ ''
-    #!${lib.getExe pkgs.fish}
-    
-    set -xa PATH ${lib.replaceStrings [":"] [" "] (lib.makeBinPath (with pkgs; [
-      jq
-      curl
-      gawk
-    ]))}
+pkgs.writeScriptBin "update-firefox" /* fish */ ''
+  #!${lib.getExe pkgs.fish}
+  
+  set -xa PATH ${lib.replaceStrings [":"] [" "] (lib.makeBinPath (with pkgs; [
+    jq
+    curl
+    gawk
+  ]))}
 
-    ${lib.fileContents ./update.fish}
-  '';
-  installPhase = ''
-    mkdir $out/bin
-    cp -rf bin/* $out/bin/
-  '';
-}
+  ${lib.fileContents ./update.fish}
+''
