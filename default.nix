a5514a3ca9955862ac120811c8c452b34bc98e89
fmway <fm18lv@gmail.com>
2024-11-30 13:46:09 +0700

    fix(firefox-addons): kesalahan berfikir

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@
 in {
   bluetui = callPackage ./bluetui {};
   firefox-addons = import ./firefox-addons { inherit pkgs lib; };
-  firefoxAddonsUpdater = callPackage ./firefox-addons/firefoxAddonsUpdater.nix;
+  firefoxAddonsUpdater = callPackage ./firefox-addons/firefoxAddonsUpdater.nix {};
   mpv-scripts = let
     buildLua = import ./mpv-scripts/buildLua.nix { inherit pkgs lib; };
   in {
--- firefox-addons/firefoxAddonsUpdater.nix
+++ firefox-addons/firefoxAddonsUpdater.nix
@@ -1,12 +1,19 @@
 { pkgs, lib, ... }:
-pkgs.writeScriptBin /* fish */ ''
-  #!${lib.getExe pkgs.fish}
-  
-  set -xa PATH ${lib.replaceStrings [":"] [" "] (lib.makeBinPath (with pkgs; [
-    jq
-    curl
-    gawk
-  ]))}
+pkgs.stdenvNoCC.mkDerivation {
+  name = "firefox-updater";
+  src = pkgs.writeScriptBin /* fish */ ''
+    #!${lib.getExe pkgs.fish}
+    
+    set -xa PATH ${lib.replaceStrings [":"] [" "] (lib.makeBinPath (with pkgs; [
+      jq
+      curl
+      gawk
+    ]))}
 
-  ${lib.fileContents ./update.fish}
-''
+    ${lib.fileContents ./update.fish}
+  '';
+  installPhase = ''
+    mkdir $out/bin
+    cp -rf bin/* $out/bin/
+  '';
+}
