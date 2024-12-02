db61d62280330aeee9e35ce6d8f07d300f9a60b2
fmway <fm18lv@gmail.com>
2024-12-02 11:33:48 +0700

    fix: allow insecure dotnet

--- flake.nix
+++ flake.nix
@@ -14,6 +14,14 @@
       inherit pkgs lib;
     };
   }) // {
+    nixpkgs.config = {
+      permittedInsecurePackages = [
+        "dotnet-runtime-wrapped-6.0.36"
+        "dotnet-runtime-6.0.36"
+        "dotnet-sdk-wrapped-6.0.428"
+        "dotnet-sdk-6.0.428"
+      ];
+    };
     overlays.default = import ./overlay.nix;
   };
 }
