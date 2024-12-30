2b8218bdead38290d461eec42ddaf655908f51af
fmway <fm18lv@gmail.com>
2024-12-30 20:05:47 +0700

    chore: still fucking

--- bluetui/update.fish
+++ bluetui/update.fish
@@ -1,13 +1,13 @@
 #!/usr/bin/env -S nix shell nixpkgs#expect nixpkgs#nix-init nixpkgs#fish --command fish
 expect (echo 'spawn nix-init -u https://github.com/pythops/bluetui ./bluetui/default.nix
 expect "Enter tag or revision *"
-send "\\r"
+send "\r"
 expect "Enter versio*"
-send "\\r"
+send "\r"
 expect "Enter pnam*"
-send "\\r"
+send "\r"
 expect "How should this package be bui*"
-send "\\r"
+send "\r"
 expect "Overwrite*"
-send "\\r"
+send "\r"
 interact' | psub)
