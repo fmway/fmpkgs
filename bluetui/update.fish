879dcaae92428750c3ffe8e209bb538f4000c9d4
fmway <fm18lv@gmail.com>
2024-12-30 19:59:58 +0700

    chore(actions): 🖕

--- bluetui/update.fish
+++ bluetui/update.fish
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S nix shell nixpkgs#expect nixpkgs#nix-init --command fish
+#!/usr/bin/env -S nix shell nixpkgs#expect nixpkgs#nix-init nixpkgs#fish --command fish
 expect (echo 'spawn nix-init -u https://github.com/pythops/bluetui ./bluetui/default.nix
 expect "Enter tag or revision *"
 send "\\r"
