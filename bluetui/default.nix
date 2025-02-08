2f4d8b94512bf5317989aade0355a7d8dcb21eaa
xdman_bot <fm18lv@gmail.com>
2025-02-08 12:12:27 +0000

    chore(pkgs): update some package

--- bluetui/default.nix
+++ bluetui/default.nix
@@ -16,7 +16,7 @@ rustPlatform.buildRustPackage rec {
     hash = "sha256-JgxzpFpz/fyFZwyxTtAkG9XB5qkxj46lUnZ3mM44dHk=";
   };
 
-  cargoHash = "sha256-F0dYE1Kd+QDOYbhYTxBub6tS1MMZTjGqTJSQUSxpAP4=";
+  cargoHash = "sha256-i0hlCMYpfllI3BiNq2yS1YgSrp9GldzOjJMQ8dKFlmU=";
 
   nativeBuildInputs = [
     pkg-config
