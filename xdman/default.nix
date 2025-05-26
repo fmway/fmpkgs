ca0929baac8f39c5079fb1d9a12d143c21a79cae
fmway <fm18lv@gmail.com>
2025-05-27 04:48:54 +0700

    chore(xdman): expose chrome-extension

--- xdman/default.nix
+++ xdman/default.nix
@@ -41,18 +41,20 @@
   installPhase = ''
     runHook preInstall
 
-    mkdir -p $out/share/applications $out/bin $out/share/icons/hicolor/512x512/apps/ $out/share/icons/hicolor/scalable/apps
+    mkdir -p $out/share/applications $out/bin $out/share/icons/hicolor/512x512/apps/ $out/share/icons/hicolor/scalable/apps $out/share/xdman/chrome-extension
     ln -sv ${xdm-gtk}/bin/xdm-app $out/bin/${pname}
     install -m 644 ${xdm-gtk}/lib/xdm-gtk/xdm-logo-512.png $out/share/icons/hicolor/512x512/apps/xdm-logo.png
     install -m 644 ${xdm-gtk}/lib/xdm-gtk/xdm-logo-512.png $out/share/icons/hicolor/512x512/apps/xdm-app.png
     install -m 644 ${xdm-gtk}/lib/xdm-gtk/xdm-logo.svg $out/share/icons/hicolor/scalable/apps/xdm-logo.svg
     install -m 644 ${xdm-gtk}/lib/xdm-gtk/xdm-logo.svg $out/share/icons/hicolor/scalable/apps/xdm-app.svg
 
+    cp ${xdm-gtk}/lib/xdm-gtk/chrome-extension/* $out/share/xdman/chrome-extension/
+
     runHook postInstall
   '';
 
   meta = with lib; {
-    description = "Powerfull download accelerator and video downloader";
+    description = "Powerful download accelerator and video downloader";
     homepage = "https://github.com/subhra74/xdm";
     license = licenses.gpl2Only;
     # maintainers = with maintainers; [ ];
