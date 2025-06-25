ad329f8e6b5ae32700341d8fba9c48d8f4db2ead
xdman_bot <fm18lv@gmail.com>
2025-06-25 12:01:16 +0000

    chore(firefox-addons): update some addons

--- firefox-addons/generated_addons.nix
+++ firefox-addons/generated_addons.nix
@@ -47,10 +47,10 @@ in {
   };
   stayfree = buildFirefoxXpiAddon {
     pname = "stayfree";
-    version = "2.2.11";
+    version = "2.2.12";
     addonId = "{30b15d56-b2fa-4cb2-98fd-7b5e26306483}";
-    url = "https://addons.mozilla.org/firefox/downloads/file/4514299/stayfree-2.2.11.xpi";
-    sha256 = "791073be1aa3fd8d5dea0d8065df4632b052cd2182452e057d4a938e3f6fbf6f";
+    url = "https://addons.mozilla.org/firefox/downloads/file/4520046/stayfree-2.2.12.xpi";
+    sha256 = "6e8c6654cd53131d25fb330e1fe94003ffaea88e8f9d71d4fa4214a335f9785c";
     meta = {
       homepage = "https://stayfreeapps.com/";
       mozPermissions = [
